bcff3a73c0ba12c34b3f33655a07db2681c7540d
Tony123-tech <tony1225leung@gmail.com>
2025-08-16 13:05:13 +0800

    Create AdSkipper.js

---
+++ AdSkipper.js
@@ -0,0 +1,9 @@
+/**
+* CyberYube Ad Skipper - Compiled JavaScript Module
+* Automatically detects and skips video advertisements
+*/
+class CyberAdSkipper {
+constructor(config ={}) {
+this.observer = null;
+}
+}
