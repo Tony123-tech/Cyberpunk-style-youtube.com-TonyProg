fa4a070df2d2b94390f202871e684033bceee281
GitHub one person <tony1225leung@gmail.com>
2025-08-30 13:26:39 +0800

    video.vb

---
+++ video.vb
@@ -0,0 +1,9 @@
+' Models/Video.vb
+Public Class Video
+Public Property Id As Integer
+Public Property Title As String
+Public Property Description As String
+Public Property FilePath As String
+Public Property YouTubeUrl As String
+Public Property UploadDate As Datetime
+End Class
